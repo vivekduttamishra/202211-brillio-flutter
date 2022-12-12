007ea4d2e2cdd110549b77181a7a025a0e668874
Vivek Dutta Mishra <vivek@conceptarchitect.in>
2022-12-12 12:30:15 +0530

    day#1 update#1

---
+++ dart-demos/02datatype.dart
@@ -0,0 +1,13 @@
+void main(){
+
+  int x=20;
+
+  int y=30;
+
+  double z=x/y;
+
+  print("dividing "+x.toString()+ " by "+y.toString()+" returns "+z.toString());
+}
+
+
+
---
+++ dart-demos/03string-interpolation.dart
@@ -0,0 +1,18 @@
+
+
+void start(){
+  print("you are welcome to dart!!!");
+}
+
+void main(){
+
+  int x=20;
+
+  int y=30;
+
+  start();
+  print("dividing $x by $y returns ${x/y} ");
+}
+
+
+
